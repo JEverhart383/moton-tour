af7852b215233dd4b333d7e5f3766f8213524428
Jeff Everhart <jeffeverhart383@gmail.com>
2016-08-18 21:16:22 -0400

    update stops on tour w/ coords

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,175 @@
 
+/****
+    Initialize Map and Add Layer from MapBox
+***/
+
+
 var mymap = L.map('map').setView([37.30266489591309, -78.39217185974121], 14.5);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiamVmZmV2ZXJoYXJ0MzgzIiwiYSI6IjIwNzVlOTA3ODI2MTY0MjM3OTgxMTJlODgzNjg5MzM4In0.QA1GsfWZccIB8u0FbhJmRg', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18,
+    maxZoom: 19,
     id: 'jeffeverhart383.po06polc',
     accessToken: 'pk.eyJ1IjoiamVmZmV2ZXJoYXJ0MzgzIiwiYSI6IjIwNzVlOTA3ODI2MTY0MjM3OTgxMTJlODgzNjg5MzM4In0.QA1GsfWZccIB8u0FbhJmRg'
 }).addTo(mymap);
+
+
+/****
+    Add Markers
+***/
+
+var wayPoints = [
+    {
+        name: "Moton Musuem", 
+        lat: 37.29168827648954, 
+        long: -78.39762319181799, 
+        desc: "", 
+        link: "#"
+    },
+    {
+        name: "Mary E. Branch Community Center/School", 
+        lat: 37.29327304292218, 
+        long: -78.39563350200504, 
+        desc: "", 
+        link: "#"
+    }, 
+    {
+        name: "Farmville Shopping Center/Dr. Miller's Office", 
+        lat: 37.299878736992646, 
+        long: -78.3936507628098, 
+        desc: "", 
+        link: "#"
+    }, 
+    {
+        name: "Beulah AME Church & Parsonage", 
+        lat: 37.300961930339504, 
+        long: -78.39330232709496, 
+        desc: "", 
+        link: "#"
+    }, 
+    {
+        name: "First Baptist Church", 
+        lat: 37.30078817348676, 
+        long: -78.39320987470298, 
+        desc: "", 
+        link: "#"
+    }, 
+    {
+        name: "State Theater", 
+        lat: 37.30110869746404, 
+        long: -78.39307894937544, 
+        desc: "", 
+        link: "#"
+    }, 
+    {
+        name: "College Shoppe", 
+        lat: 37.30148106351243, 
+        long: -78.39281466796844, 
+        desc: "", 
+        link: "#"
+    }, 
+    {
+        name: "Prince Edward County Courthouse", 
+        lat: 37.30185342956083, 
+        long: -78.39254963219014, 
+        desc: "", 
+        link: "#"
+    }, 
+    {
+        name: "Farmville Baptist Church", 
+        lat: 37.302071694319416, 
+        long: -78.39243764996377, 
+        desc: "", 
+        link: "#"
+    }, 
+    {
+        name: "J.J. Newberry's", 
+        lat: 37.3027297156279, 
+        long: -78.3926264104232, 
+        desc: "", 
+        link: "#"
+    }, 
+    {
+        name: "Presbyterian Church", 
+        lat: 37.30295745193707, 
+        long: -78.3934311569467, 
+        desc: "", 
+        link: "#"
+    }, 
+     {
+        name: "Farmville <i>Herald</i> offices", 
+        lat: 37.30218082669871, 
+        long: -78.39359091602115, 
+        desc: "", 
+        link: "#"
+    }, 
+    {
+        name: "Farmville Methodist Church/Confederate Statue", 
+        lat: 37.30179739653813, 
+        long: -78.395375255556833, 
+        desc: "", 
+        link: "#"
+    }, 
+    {
+        name: "John's Memorial Episcopal Church, Parish House/Gordon Moss Home", 
+        lat: 37.30160939044999, 
+        long: -78.39598101571053, 
+        desc: "", 
+        link: "#"
+    }, 
+    {
+        name: "Jarman", 
+        lat: 37.30148198552178, 
+        long: -78.39786568663865, 
+        desc: "", 
+        link: "#"
+    },
+    {
+        name: "Race Street/Forrester", 
+        lat: 37.2965972638105, 
+        long: -78.39862416505665, 
+        desc: "", 
+        link: "#"
+    },
+    {
+        name: "Rev. Frances L. Griffin home/E.B. Allen Funeral Home", 
+        lat: 37.29388383220629, 
+        long: -78.39826818562895, 
+        desc: "", 
+        link: "#"
+    }
+    ]
+
+function createMapMarkers(arrayOfWayPoints){
+
+    for (var i = 0; i < arrayOfWayPoints.length; i ++){
+            L.marker([arrayOfWayPoints[i].lat, arrayOfWayPoints[i].long]).addTo(mymap)
+             .bindPopup(arrayOfWayPoints[i].name);
+
+
+    }
+
+}
+
+//call to create markers 
+
+createMapMarkers(wayPoints); 
+
+
+//create circle at moton
+
+var userPositionCircle = L.circle([wayPoints[0].lat, wayPoints[0].long]).addTo(map); 
+
+
+
+function onLocationFound(e) {
+   // var radius = e.accuracy / 2;
+
+   // if (!)
+   //  L.circle(e.latlng, radius).addTo(map);
+}
+
+mymap.locate({watch:true, enableHighAccuracy:true}); 
+
+
+mymap.on('locationfound', onLocationFound);    
